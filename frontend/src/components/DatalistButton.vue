52acd9617fbedf3982ba0819a11c123f846bfa9f
Yann Carvalho <yanncarvalho@outlook.com>
2022-08-23 16:15:10 -0300

    feat: create reactive on click element

---
+++ frontend/src/components/DatalistButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  text: String,
+});
+</script>
+
+<template>
+  <button
+    type="button"
+    class="btn bg-chosen cursor-chosen m-1 p-2 pe-lg-5 ps-lg-5 rounded-4"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<style setup>
+.bg-chosen:hover {
+  background-color: var(--color-primary-100) !important;
+}
+.bg-chosen {
+  background-color: var(--color-primary-70) !important;
+}
+</style>
--- frontend/src/components/TheDatalist.vue
+++ frontend/src/components/TheDatalist.vue
@@ -1,36 +1,51 @@
-<script setup></script>
+<script setup>
+import DatalistButton from "./DatalistButton.vue";
+import { ref } from "vue";
+let id = 0;
+let iCountry = 0;
+const elements = ref([
+  { id: id++, text: "Brazil" },
+  { id: id++, text: "Chile" },
+  { id: id++, text: "Colombia" },
+]);
+
+const country = ref([
+  { id: iCountry++, text: "Chile" },
+  { id: iCountry++, text: "Colombia" },
+]);
+
+function removeElement(element) {
+  elements.value = elements.value.filter((e) => e !== element);
+}
+</script>
 
 <template>
-  <div>
-    <div class="container pb-4">
+  <div class="container pb-4">
+    <div>
       <label for="countryDataList" class="form-label m-0">Country</label>
       <input
-        class="form-select bg-datalist rounded-3 p-2 text-center"
+        class="form-select bg-datalist rounded-3 p-2 me-1"
         list="countryOptions"
         id="countryDataList"
         placeholder="Choose a country..."
       />
 
       <datalist id="countryOptions">
-        <option value="Brazil"></option>
-        <option value="Chile"></option>
-        <option value="Colombia"></option>
+        <option v-for="coun in country" :key="coun.id">{{ coun.text }}</option>
       </datalist>
-    </div>
-    <div
-      class="container d-flex flex-row mb-3 justify-content-evenly text-center"
-    >
-      <div class="bg-chosen p-2 pe-lg-5 ps-lg-5 rounded-4 align-self-center">
-        aaa
-      </div>
+      <ul class="container d-flex flex-wrap justify-content-evenly text-center">
+        <DatalistButton
+          v-for="element in elements"
+          :key="element.id"
+          :text="element.text"
+          @click="removeElement(element)"
+        />
+      </ul>
     </div>
   </div>
 </template>
 
 <style setup>
-.bg-chosen {
-  background-color: var(--color-primary-70);
-}
 .bg-datalist {
   background-color: var(--color-primary-100) !important;
 }
